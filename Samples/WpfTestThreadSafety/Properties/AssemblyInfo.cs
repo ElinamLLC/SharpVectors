6e218d6725662b8aca610aba8d7cab10e97c12ef
paulushub <paulushub@gmail.com>
2019-10-29 06:20:55 +0900

    Preparing New Version Release 1.5.0.0
    - Version information update
    - Adding nuget package
    - Nuget package now include PDB files

--- Samples/WpfTestThreadSafety/Properties/AssemblyInfo.cs
+++ Samples/WpfTestThreadSafety/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("WpfTestThreadSafety")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Elinam LLC, Japan")]
 [assembly: AssemblyProduct("WpfTestThreadSafety")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyCopyright("Copyright © 2010 - 2019 Elinam LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
