b031cc6da1a12f8100e428e432754cd065045c14
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-06-20 17:28:36 +0900

    add docs for PathUtils

--- Source/SharpVectorCore/Utils/PathUtils.cs
+++ Source/SharpVectorCore/Utils/PathUtils.cs
@@ -4,8 +4,17 @@ using System.Reflection;
 
 namespace SharpVectors.Dom.Utils
 {
+	/// <summary>
+	/// Extends functionality of <see cref="Path"/> and provides additional methods for manipulating file or directory path information
+	/// </summary>
 	public static class PathUtils
 	{
+		/// <summary>
+		/// Combines an assembly location and an array of strings into a path
+		/// </summary>
+		/// <param name="assembly">Assembly which is taken as the base path</param>
+		/// <param name="paths">Path segments which are appended to the assembly location</param>
+		/// <returns>The contained paths</returns>
 		public static string Combine(Assembly assembly, params string[] paths)
 		{
 			var location = assembly.Location;
