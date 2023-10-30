27f0438e41e427f4217f4b73a4a8077334f6e035
paulushub <paulushub@users.noreply.github.com>
2023-10-30 16:49:30 +0000

    deploy: d222cb60227346ab12847c4624f2047d3470b51e

---
+++ public/main.js
@@ -0,0 +1,19 @@
+/**
+ * Licensed to the .NET Foundation under one or more agreements.
+ * The .NET Foundation licenses this file to you under the MIT license.
+ */
+
+export default {
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/ElinamLLC/SharpVectors',
+      title: 'GitHub: SharpVectors'
+    },
+    {
+      icon: 'filetype-svg',
+      href: 'https://developer.mozilla.org/en-US/docs/Web/SVG',
+      title: 'SVG: Scalable Vector Graphics'
+    }
+  ]
+}
