59aef849b873bd98ab285c4a05ed0535104fbacc
paulushub <paulushub@gmail.com>
2024-08-13 10:53:53 +0900

    Release 1.8.4.2

--- Source/SharpVectorDom/Properties/AssemblyInfo.cs
+++ Source/SharpVectorDom/Properties/AssemblyInfo.cs
@@ -30,9 +30,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.8.4.1")]
-[assembly: AssemblyFileVersion("1.8.4.1")]
-[assembly: AssemblyInformationalVersion("1.8.4.1")]
+[assembly: AssemblyVersion("1.8.4.2")]
+[assembly: AssemblyFileVersion("1.8.4.2")]
+[assembly: AssemblyInformationalVersion("1.8.4.2")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
